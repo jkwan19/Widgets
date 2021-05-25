f263523ec4626e8a7e1bc15428d7808e2535fa1b
Justin Kwan <jkwan19@gmail.com>
2021-05-25 16:20:45 -0700

    Display list of items

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Accordion from './Accordion';
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework"
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite javascript library amongst engineers"
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components"
+  }
+];
 
 const App = () => {
   return (
-    <div> Accordian</div>
+    <div>
+      <Accordion items={items} />
+    </div>
   )
 }
 
