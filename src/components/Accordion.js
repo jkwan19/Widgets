0aa8965d625cd946643ae9ac9edaa49144fff036
Justin Kwan <jkwan19@gmail.com>
2021-05-25 16:36:52 -0700

    Implement active dropdown onclick

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {
+  useState,
+} from 'react';
 
 const Accordion = ({ items }) => {
 
-  const renderedItems = items.map((item) => {
+  const [ activeIndex, setActiveIndex ] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index)
+  };
+
+  const renderedItems = items.map((item, index) => {
+
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div
+          className={index === activeIndex ? "title active" : "title"}
+          onClick={() => onTitleClick(index)}
+          >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={index === activeIndex ? "content active" : "content"}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
