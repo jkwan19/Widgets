e5e9fae7c26c74b59058de8e2282499cd2206de8
Justin Kwan <jkwan19@gmail.com>
2021-05-25 17:10:58 -0700

    Request from wikipedia API

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,28 @@
 import React, {
-  useState
+  useState,
+  useEffect
 } from 'react';
+
+import axios from 'axios';
 
 const Search = () => {
 
   const [ term, setTerm ] = useState('');
+
+  useEffect(() => {
+    const search = async() => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      })
+    };
+    search();
+  }, [term]);
 
   return (
     <div>
