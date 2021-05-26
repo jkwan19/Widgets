d0ca92c8b848e0120eab7b997af5d4b2cc1b8020
Justin Kwan <jkwan19@gmail.com>
2021-05-25 22:03:14 -0700

    Set default search term and set list of data

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,11 +7,12 @@ import axios from 'axios';
 
 const Search = () => {
 
-  const [ term, setTerm ] = useState('');
+  const [ term, setTerm ] = useState('programming');
+  const [ results, setResults ] = useState([]);
 
   useEffect(() => {
     const search = async() => {
-      await axios.get('https://en.wikipedia.org/w/api.php', {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
@@ -20,6 +21,8 @@ const Search = () => {
           srsearch: term
         }
       })
+
+      setResults(data.query.search);
     };
     search();
   }, [term]);
