7f4e5324916d896c9c408d9bff8534544b5f464f
Justin Kwan <jkwan19@gmail.com>
2021-05-28 16:28:03 -0700

    Access google translate api

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Convert = ({
   language,
   text
 }) => {
 
+  const [ translatedText, setTranslatedText ] = useState('');
+
   useEffect(() => {
-    console.log('new language/text')
+    const response = axios.post('https://translation.googleapis.com/language/translate/v2',{},{
+      params: {
+        q: text,
+        target: language.value,
+        key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+      }
+    })
   }, [language, text]);
 
   return (
