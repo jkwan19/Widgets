8c600ba94a78a20547ad347b0b30a6fb974a65a2
Justin Kwan <jkwan19@gmail.com>
2021-05-28 16:31:34 -0700

    Display translated text

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -6,21 +6,27 @@ const Convert = ({
   text
 }) => {
 
-  const [ translatedText, setTranslatedText ] = useState('');
+  const [ translated, setTranslated ] = useState('');
 
   useEffect(() => {
-    const response = axios.post('https://translation.googleapis.com/language/translate/v2',{},{
-      params: {
-        q: text,
-        target: language.value,
-        key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-      }
-    })
+    const doTranslate = async () => {
+      const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2',{},{
+        params: {
+          q: text,
+          target: language.value,
+          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+        }
+      })
+
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslate();
   }, [language, text]);
 
   return (
     <div>
-
+      {translated}
     </div>
   )
 }
